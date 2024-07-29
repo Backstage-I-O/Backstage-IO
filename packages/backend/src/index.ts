ed64245910623361e2aa3d5e7906df7ea314c9b1
Mohammad Ali <mohammad.ali@1und1.de>
2024-07-29 17:10:13 +0200

    Scaffoldings initiated, User Unknown Error Still persists

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -7,6 +7,10 @@
  */
 
 import { createBackend } from '@backstage/backend-defaults';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
+
+// Configuration that depends on environment variables
 
 const backend = createBackend();
 
@@ -20,12 +24,17 @@ backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'))  
+// IMPORTANT for github error resolution
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-events-backend/alpha'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
